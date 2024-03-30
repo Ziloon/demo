ec46b968a851e4ac642ef54dddc211ef2c196b7e
Zhao Ziloong <ziloon.z@icloud.com>
2024-03-30 22:26:49 +0800

    带参数方法

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,21 @@ impl Cubox {
     fn area(&self) -> u32 {
         self.width * self.length
     }
+    fn can_hold(&self, other: &Cubox) -> bool {
+        self.width >= other.width && self.length >= other.length
+    }
 }
 fn main() {
-    let rect = Cubox {
+    let rect1 = Cubox {
         width: 30,
         length: 50,
     };
-    println!("{}", rect.area());
-    println!("{:#?}", rect);
+    println!("{}", rect1.area());
+    println!("{:#?}", rect1);
+    let rect2 = Cubox {
+        width: 30,
+        length: 50,
+    };
+    println!("{}", rect1.can_hold(&rect2))
 }
 
